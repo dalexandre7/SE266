d68d9bd1010ce2df14eddf19ff19260a2bdf243b
Alexandre <dcalexandre@email.neit.edu>
2023-11-07 14:59:15 -0500

    done

---
+++ week3/ATM Starter Code/account.php
@@ -0,0 +1,59 @@
+<?php
+// This is the base class for checking and savings accounts
+// It is declared **abstract** so that it can not be instantiated
+// Child classes must be derived that 
+// implement the withdrawal and getAccountDetails methods
+
+/* Note:
+	You should implement all other methods in the class
+*/
+
+abstract class Account 
+{
+	protected $accountId;
+	protected $balance;
+	protected $startDate;
+	
+	public function __construct ($id, $bal, $startDt) 
+	{
+	  $this ->accountId = $id; 
+	  $this ->balance = $bal; 
+	  $this ->startDate = $startDt; 
+	} // end constructor
+
+	
+	public function deposit ($amount) 
+	{
+		$this ->balance = $amount + $balance; 
+		return $this ->balance; 
+	} // end deposit
+
+	// This is an abstract method. 
+	// This method must be defined in all classes
+	// that inherit from this class
+	abstract public function withdrawal($amount);
+	
+	public function getStartDate() 
+	{
+		return $this ->startDate; 
+	} // end getStartDate
+
+	public function getBalance() 
+	{
+		return $this ->balance;
+	} // end getBalance
+
+	public function getAccountId() 
+	{
+		return $this ->accountId;
+	} // end getAccountId
+
+	// Display AccountID, Balance and StartDate in a nice format
+	protected function getAccountDetails()
+	{
+		echo "AccountID" . $this->accountId . "<br>" . "Balence" . $this->balance . "<br>". "Account Opened: " . $this->accountId . "<br>";
+	} // end getAccountDetails
+	
+} // end account
+
+?>
---
+++ week3/ATM Starter Code/savings.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "./account.php";
+ 
+class SavingsAccount extends Account 
+{
+
+	public function withdrawal($amount) 
+	{
+		
+		const OVERDRAW_LIMIT = 0;
+		if($this ->balance - $amount > self::OVERDRAW_LIMIT){
+			return $this ->balance  - $amount; 
+
+		}
+		else 
+		{
+			echo "Overdrafted! Account has hit 0...Your Broke hahahaha"
+		}
+	} //end withdrawal
+
+	public function getAccountDetails() 
+	{
+		$accountDetails = "<h2>Savings Account</h2>";
+		$accountDetails .= parent::getAccountDetails();
+		
+		return $accountDetails;
+	} //end getAccountDetails
+	
+} // end Savings
+
+// The code below runs everytime this class loads and 
+// should be commented out after testing.
+    
+?>
